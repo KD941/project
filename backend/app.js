937e750ed228b0ba633825064a3c088b377212ac
Karmdip Rathva <karmdiprathva9@gmail.com>
2025-06-05 15:14:50 +0530

    added 2 pages

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,25 @@
 const libExpress = require('express');
 const libCors = require('cors');
-const libRandom=require('randomstring');
-const {MongoClient}= require('mongodb');
-const { ObjectId}=require('mongodb');
-const server= libExpress();
+const libRandom = require('randomstring');
+const { MongoClient } = require('mongodb');
+const { ObjectId } = require('mongodb');
+const server = libExpress();
 server.use(libExpress.json());
-const client= new MongoClient('mongodb://Admin:Karmdip123@localhost:27017/IMS?authSource=IMS');
+const client = new MongoClient('mongodb://Admin:Karmdip123@localhost:27017/IMS?authSource=IMS');
 
 server.use(libCors());
 //for signup
-server.post('/user/signup',async(req,res)=>
-{
-    if(req.body.name && req.body.email&&req.body.password&&req.body.phone)
-    {await client.connect();
-        const db= await client.db('IMS');
+server.post('/user/signup', async (req, res) => {
+    if (req.body.name && req.body.email && req.body.password && req.body.phone) {
+        await client.connect();
+        const db = await client.db('IMS');
         const collection = await db.collection('users');
-        const result = await collection.find({email: req.body.email}).toArray();
-        if(result.length>0)
-        {
-            res.json({message:"User already exists"});
+        const result = await collection.find({ email: req.body.email }).toArray();
+        if (result.length > 0) {
+            res.json({ message: "User already exists" });
             console.log("User already exists");
         }
-        else{
+        else {
             const user =
             {
                 name: req.body.name,
@@ -30,72 +28,117 @@ server.post('/user/signup',async(req,res)=>
                 phone: req.body.phone
             }
             await collection.insertOne(user);
-            res.json({message:"User Created Successfully"});
-console.log("User Created Successfully");
+            res.json({ message: "User Created Successfully" });
+            console.log("User Created Successfully");
 
         }
         client.close();
     }
-    else
-    {
-        res.json({message:"All fields required"});
+    else {
+        res.json({ message: "All fields required" });
     }
 })
-server.get("user/roles",async(req,res)=>
-{
+server.get("/user/roles", async (req, res) => {
     await client.connect();
-    const db= await client.db('IMS');
-    const collection = await db.collection('roles');
-    const result = await collection.find({token:req.header.token}).toArray();
-    if(result.length>0)
-    { const user=result[0];
+    const db = await client.db('IMS');
+    const collection = await db.collection('users');
+    const result = await collection.find({ token: req.headers.token }).toArray();
+    if (result.length > 0) {
+        const user = result[0];
+        console.log("User roles fetched successfully");
         res.status(200).json({
-            admin:user?.is_admin===true,
-            team_owner:!!user?.owner_of,
-            player:!!user?.playing_for
+            admin: user?.is_admin === true,
+            team_owner: !!user?.owner_of,
+            player: !!user?.playing_for
 
         })
+        client.close();
     }
-    else{
-        res.status(404).json({message:"No roles found"});
+    else {
+        res.status(404).json({ message: "No roles found" });
     }
     client.close();
 }
 )
 //for login(creating tokens)
-server.post("/token",async (req,res)=>
-{
-    if(req.body.email && req.body.password)
-    {console.log("Login request received");
+server.post("/token", async (req, res) => {
+    if (req.body.email && req.body.password) {
+        console.log("Login request received");
         await client.connect();
-        const db= await client.db('IMS');
+        const db = await client.db('IMS');
         const collection = await db.collection('users');
-        const result = await collection.find({email: req.body.email,password:req.body.password}).toArray();
-        if(result.length>0)
-        {
-            const token= libRandom.generate(7);
-            const user=result[0];
+        const result = await collection.find({ email: req.body.email, password: req.body.password }).toArray();
+        if (result.length > 0) {
+            const token = libRandom.generate(7);
+            const user = result[0];
             await collection.updateOne(
-                {_id:new ObjectId(user._id)},
-                {$set: {token: token}}
+                { _id: new ObjectId(user._id) },
+                { $set: { token: token } }
             );
-      
+
             res.status(200).json({
                 message: 'Login Successful',
                 token: token
-               
+
             });
             console.log("Login Successful");
             client.close();
         }
-        else{
-            res.status(401).json({message:"Invalid Credentials"});
+        else {
+            res.status(401).json({ message: "Invalid Credentials" });
         }
     }
-    else{
-        res.status(400).json({error:"ALL FIELDS REQUIRED"});
+    else {
+        res.status(400).json({ error: "ALL FIELDS REQUIRED" });
     }
 })
-server.listen(8000,()=>{
+server.get("/player", async (req, res) => {
+    await client.connect();
+    const db = await client.db('IMS');
+    const collection = await db.collection('users');
+    const result = await collection.find({ "playing_for":{ $exists:true}}).toArray();
+    if (result.length > 0) {
+        res.status(200).json(result);
+        console.log("Players fetched successfully");
+        console.log(result);
+    }
+    else {
+        res.status(404).json({ message: "No players found" });
+    }
+    client.close();
+})
+server.get("/teams",async (req,res)=>
+{
+    await client.connect();
+    const db = await client.db('IMS');
+    const collection = await db.collection('teams');
+    const result = await collection.find().toArray();
+    if (result.length > 0) {
+        res.status(200).json(result);
+        console.log("Teams fetched successfully");
+        console.log(result);
+    }
+    else {
+        res.status(404).json({ message: "No teams found" });
+    }
+    client.close();
+})
+server.get("player/stats", async (req,res)=>
+{
+    await client.connect();
+    const db = await client.db('IMS');
+    const collection = await db.collection('users');
+    const result = await collection.find({"_id":req.headers.id}).toArray();
+    if (result.length > 0) {
+     res.status(200).json(result[0]);
+        console.log("Player stats fetched successfully");
+        
+    }
+    else {
+        res.status(404).json({ message: "No players found" });
+    }
+    client.close();
+})
+server.listen(8000, () => {
     console.log("Server is running on port 8000");
 })
